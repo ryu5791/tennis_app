2ba16e9b86da7a80b7ec3b15655c8fdaeb5e7586
ryu5791 <ryu5791@yahoo.co.jp>
2019-10-25 03:50:17 +0900

    入力画面組み込み

--- score/urls.py
+++ score/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from . import views_view
+from . import views_view, views_input
 
 urlpatterns = [
 	path("", views_view.ranking, name="ranking"),
 	path("daily", views_view.daily, name="daily"),
+	path("input", views_input.inputScr, name="inputScr"),
+	path("import", views_input.importScr, name="importScr"),
 ]
 
---
+++ score/views_input.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+import logging
+
+@login_required(login_url='/admin/login/')
+def inputScr(request):
+    test = User.objects.filter(username=request.user)
+    logging.debug("index user:" + str(request.user))
+    logging.debug("index is_super:" + str(request.user.is_superuser))
+    logging.debug("index test:" + str(test))
+    logging.debug("index has_perm:" + str(request.user.has_perm("score.add_tblscore") ))
+#   logging.debug("index has_perm:" + str(request.user.has_perm("auth.add_user") ))
+    params = {'authInput': request.user.has_perm("score.add_tblscore"),}
+    return render(request, "score/input_input.html", params)
+
+@login_required(login_url='/admin/login/')
+def importScr(request):
+    params = {'authInput': request.user.has_perm("score.add_tblscore"),}
+    return render(request, "score/input_import.html", params)
+
